637cbb02d5c11b011b986e616602035306369e29
Jade Cong <jade.cong@qq.com>
2025-07-09 19:43:55 +0800

    fix bugs

--- robot_teleoperation/launch/robot_teleoperation.launch.py
+++ robot_teleoperation/launch/robot_teleoperation.launch.py
@@ -15,13 +15,15 @@
 
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
     return LaunchDescription([
         # Launch robot_teleoperation node
         ExecuteProcess(
-            cmd=['../scripts/robot_teleoperation.sh'],
+            cmd=[os.path.join(get_package_share_directory('robot_teleoperation'), 'scripts', 'robot_teleoperation.sh')],
             shell=True,
             output='screen'
         ),
