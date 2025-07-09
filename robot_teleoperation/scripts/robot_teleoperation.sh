ec8a37994644fb24449b9771256f1f4ebc5cc1f0
Jade Cong <jade.cong@qq.com>
2025-07-09 21:07:45 +0800

    fix bugs

--- robot_teleoperation/scripts/robot_teleoperation.sh
+++ robot_teleoperation/scripts/robot_teleoperation.sh
@@ -1,40 +1,49 @@
 #!/usr/bin/env bash
 
-# Enable job control
-set -m
-
 echo "Robot Teleoperation Start..."
+
+# Define cleanup function
+cleanup() {
+    echo "Caught Ctrl+C signal. Stopping all child nodes..."
+    
+    # Stop all child nodes
+    pkill -f 'ros2 launch'
+    
+    # Close all child terminal windows
+    pkill -f 'master_hfd.launch.py'
+    pkill -f 'slave_realman.launch.py'
+    pkill -f 'slave_ctek.launch.py'
+    # pkill -f 'slave_xjcsensor.launch.py'
+    # pkill -f 'slave_realsense.launch.py'
+    
+    echo "All child nodes stopped. Exiting..."
+    exit 0
+}
+
+# Set trap to catch Ctrl+C signal
+trap cleanup SIGINT
 
 # Launch master_hfd node
 echo "Launch master_hfd node..."
-gnome-terminal -- bash -c "ros2 launch hfd_teleoperation master_hfd.launch.py; exec bash" &
-PID1=$!
+gnome-terminal --title="MASTER-HFD" -- bash -c "ros2 launch hfd_teleoperation master_hfd.launch.py; exec bash"
 
 # Launch slave_realman node
 echo "Launch slave_realman node..."
-gnome-terminal -- bash -c "ros2 launch realman_teleoperation slave_realman.launch.py; exec bash" &
-PID2=$!
+gnome-terminal --title="SLAVE-REALMAN" -- bash -c "ros2 launch realman_teleoperation slave_realman.launch.py; exec bash"
 
 # Launch slave_ctek node
 echo "Launch slave_ctek node..."
-gnome-terminal -- bash -c "ros2 launch ctek_teleoperation slave_ctek.launch.py; exec bash" &
-PID3=$!
+gnome-terminal --title="SLAVE-CTEK" -- bash -c "ros2 launch ctek_teleoperation slave_ctek.launch.py; exec bash"
 
 # Launch slave_xjcsensor node
 # echo "Launch slave_xjcsensor node..."
-# gnome-terminal -- bash -c "ros2 launch xjcsensor_teleoperation slave_xjcsensor.launch.py; exec bash" &
-# PID4=$!
+# gnome-terminal --title="SLAVE-XJCSENSOR" -- bash -c "ros2 launch xjcsensor_teleoperation slave_xjcsensor.launch.py; exec bash"
 
 # Launch slave_realsense node
 # echo "Launch slave_realsense node..."
-# gnome-terminal -- bash -c "ros2 launch realsense_teleoperation slave_realsense.launch.py; exec bash" &
-# PID5=$!
+# gnome-terminal --title="SLAVE-REALSENSE" -- bash -c "ros2 launch realsense_teleoperation slave_realsense.launch.py; exec bash"
 
 # Catch Ctrl+C signal
-trap "echo 'Caught Ctrl+C signal, killing all child processes...'; kill $PID1 $PID2 $PID3 2>/dev/null; exit 0" SIGINT
-
-# Wait for all child processes to finish
-# wait
 echo "Press Ctrl+C to exit..."
 while true; do
     sleep 1
