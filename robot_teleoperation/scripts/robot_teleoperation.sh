ecbdbf8f989ec17071ed076f48d813faff1df80c
Jade Cong <jade.cong@qq.com>
2025-08-15 11:58:16 +0800

    fix bugs

--- robot_teleoperation/scripts/robot_teleoperation.sh
+++ robot_teleoperation/scripts/robot_teleoperation.sh
@@ -2,25 +2,22 @@
 
 echo "Robot Teleoperation Start..."
 
+# Declare array variable for storing all terminal pids
+declare -a terminal_pids
+
 # Define cleanup function
 cleanup() {
     echo "Caught Ctrl+C signal. Stopping all child nodes..."
     
     # Close all terminal windows
-    for pid in $(pgrep -f 'ros2 launch'); do
-        echo "Sending SIGINT to process $pid"
-        kill -SIGINT "$pid"
+    for pid in "${terminal_pids[@]}"; do
+        if ps -p $pid > /dev/null; then
+            kill -SIGINT $pid
+        fi
     done
     
     # Wait for all to exit gracefully
-    sleep 2
-    
-    # Ensure all remaining terminal windows are killed
-    pkill -f 'master_hfd.launch.py'
-    pkill -f 'slave_realman.launch.py'
-    pkill -f 'slave_ctek.launch.py'
-    pkill -f 'slave_xjcsensor.launch.py'
-    pkill -f 'slave_realsense.launch.py'
+    wait "${terminal_pids[@]}" 2 > /dev/null
     
     echo "All child nodes stopped. Exiting..."
     echo "Robot Teleoperation Done..."
@@ -32,29 +29,30 @@ trap cleanup SIGINT
 
 # Grant permission to all device ports
 echo "Grant permission to all device ports..."
-echo 156958 | sudo -S chmod 777 /dev/ttyACM0
+# echo 156958 | sudo -S chmod 777 /dev/ttyACM0
+sudo chmod 777 /dev/ttyACM0
 sudo chmod 777 /dev/ttyACM1
 sudo chmod 777 /dev/ttyUSB0
 
 # Launch slave_xjcsensor node
 echo "Launch slave_xjcsensor node..."
-gnome-terminal --title="SLAVE-XJCSENSOR" -- bash -c "ros2 launch xjcsensor_teleoperation slave_xjcsensor.launch.py; exec bash"
+gnome-terminal --title="SLAVE-XJCSENSOR" -- bash -c "ros2 launch xjcsensor_teleoperation slave_xjcsensor.launch.py; exec bash" & terminal_pids+=($!)
 
 # Launch master_hfd node
 echo "Launch master_hfd node..."
-gnome-terminal --title="MASTER-HFD" -- bash -c "ros2 launch hfd_teleoperation master_hfd.launch.py; exec bash"
+gnome-terminal --title="MASTER-HFD" -- bash -c "ros2 launch hfd_teleoperation master_hfd.launch.py; exec bash" & terminal_pids+=($!)
 
 # Launch slave_realman node
 echo "Launch slave_realman node..."
-gnome-terminal --title="SLAVE-REALMAN" -- bash -c "ros2 launch realman_teleoperation slave_realman.launch.py; exec bash"
+gnome-terminal --title="SLAVE-REALMAN" -- bash -c "ros2 launch realman_teleoperation slave_realman.launch.py; exec bash" & terminal_pids+=($!)
 
 # Launch slave_ctek node
 echo "Launch slave_ctek node..."
-gnome-terminal --title="SLAVE-CTEK" -- bash -c "ros2 launch ctek_teleoperation slave_ctek.launch.py; exec bash"
+gnome-terminal --title="SLAVE-CTEK" -- bash -c "ros2 launch ctek_teleoperation slave_ctek.launch.py; exec bash" & terminal_pids+=($!)
 
 # Launch slave_realsense node
 echo "Launch slave_realsense node..."
-gnome-terminal --title="SLAVE-REALSENSE" -- bash -c "ros2 launch realsense_teleoperation slave_realsense.launch.py; exec bash"
+gnome-terminal --title="SLAVE-REALSENSE" -- bash -c "ros2 launch realsense_teleoperation slave_realsense.launch.py; exec bash" & terminal_pids+=($!)
 
 # Catch Ctrl+C signal
 echo "Press Ctrl+C to exit..."
